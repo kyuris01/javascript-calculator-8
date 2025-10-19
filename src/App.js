e37c09972fccb551094366dbd7fca98a495a3f0a
Chris Ahn <chrisahn1007@gmail.com>
2025-10-19 22:42:10 +0900

    chore(inputreader): add jsdoc comment to InputReader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Console } from "@woowacourse/mission-utils";
 import inputReader from "./InputReader.js";
 import stringDevider from "./StringDevider.js";
 
--- src/InputReader.js
+++ src/InputReader.js
@@ -1,5 +1,9 @@
 import { Console } from "@woowacourse/mission-utils";
 
+/**
+ *
+ * @returns {string} 사용자 입력 문자열
+ */
 const inputReader = async () => {
   try {
     const input = await Console.readLineAsync("덧셈할 문자열을 입력하세요");
