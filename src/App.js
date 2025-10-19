0c1ef7cdc9a04183cd707a076e018097951c7721
Chris Ahn <chrisahn1007@gmail.com>
2025-10-19 22:21:53 +0900

    chore(overall): remove comment for debugging
    - remove some comments which are used for debugging

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ class App {
     const input = await inputReader();
 
     const regex = /^\/\/.+\\n$/; // 커스텀 구분자 설정 판별 정규표현식
-    // Console.print(regex.test(input.substring(0, 5)));
     const customDelimIndex = input.indexOf("n") + 1;
-    // Console.print(input.substring(2, customDelimIndex - 2));
     if (regex.test(input.substring(0, customDelimIndex))) {
       const testString = input.substring(customDelimIndex);
       const customDelimiter = input.substring(2, customDelimIndex - 2);
--- src/StringDevider.js
+++ src/StringDevider.js
@@ -11,7 +11,6 @@ const stringDevider = (string, delimiter) => {
   const regExp = new RegExp(`[${basicDelimiter}${customDelimiter}]`);
 
   const inputArray = string.split(regExp);
-  //   Console.print(inputArray);
   const sum = inputArray.reduce((acc, cur) => {
     if (isNaN(Number(cur))) throw new Error("[ERROR] 잘못된 사용자 입력입니다");
     if (cur < 0) throw new Error("[ERROR] 입력은 음수가 될 수 없습니다");
